c97c1317442e920118eb00eda173470fba20642e
ven0my <vaibhavm391@gmail.com>
2020-10-06 01:49:30 +0530

    created login page

--- server.py
+++ server.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_pymongo import PyMongo
+from cfg import config
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = config["mongo_uri"]
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def show_index():
+    user_documents = mongo.db.users.find({})
+    print(user_documents)
+    for doc in user_documents:
+        print(doc)
+    return 'This is my home page'
+
+@app.route('/login')
+def show_login():
+    return render_template('login.html')
+
+@app.route('/check_login', methods=['POST'])
+def check_login():
+    email = request.form['email5']
+    password = request.form['password5']
+
+    return 'email:' +email + 'password:' +password
+
+@app.route('/signup')
+def show_signup():
+    return 'this is my signup page'
 
 if __name__ == '__main__':
     app.run(debug=True)
